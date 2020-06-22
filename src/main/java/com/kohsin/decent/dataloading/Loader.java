883c9424a423c1d449d03cf5c41073857f956ba3
akn <a.kohsin@nuadu.com>
2020-06-22 13:25:17 +0200

    last

--- src/main/java/com/kohsin/decent/dataloading/Loader.java
+++ src/main/java/com/kohsin/decent/dataloading/Loader.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kohsin.decent.entities.Claim;
 import com.kohsin.decent.repository.ClaimRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,7 @@ import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.Objects;
 
@@ -22,9 +24,9 @@ public class Loader {
     private ClaimRepository claimRepository;
 
     private ObjectMapper objectMapper;
-
+    @Autowired
+    ResourceLoader resourceLoader;
     private static String filesDirectory = "/claims/";
-    private static String savedClaims = filesDirectory + "saved/";
 
     @Autowired
     public Loader(ClaimRepository claimRepository) throws URISyntaxException {
@@ -45,14 +47,14 @@ public class Loader {
     //todo: implement mechanism for moving loaded files to another directory
     private void loadDataFromFiles() {
         try {
-            Path resourcePath = Paths.get(Loader.class.getResource(filesDirectory).toURI());
-            String[] filenames = Objects.requireNonNull(resourcePath.toFile().list());
+
+            String[] filenames = Paths.get(Loader.class.getResource(filesDirectory).toURI()).toFile().list();
             for (String path : filenames) {
-                File file = Paths.get(resourcePath.toString() + '/' + path).toFile();
+                File file = new ClassPathResource(filesDirectory + '/' + path).getFile();
                 Claim claim = objectMapper.readValue(file, Claim.class);
                 saveClaimToDb(claim);
             }
-        } catch (URISyntaxException | IOException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
     }
