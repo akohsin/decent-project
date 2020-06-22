df512bc645fbc516853ff543b73c3e1547c92c0c
akn <a.kohsin@nuadu.com>
2020-06-22 08:39:16 +0200

    initial commit

---
+++ src/main/java/com/kohsin/decent/DecentApplication.java
@@ -0,0 +1,13 @@
+package com.kohsin.decent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kohsin/decent/DecentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kohsin.decent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
